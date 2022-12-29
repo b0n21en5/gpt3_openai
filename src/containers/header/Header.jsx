e4224b6a6d6b946bd63c1690d67e9c4e7496bb25
Bikash Nath <splittergame91@gmail.com>
2022-12-29 21:33:12 +0530

    containers

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header</h3>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+          <h1 className="gradient__text">Lets Build Something amazing with GPT3 OpenAI</h1>
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className="gpt3__header-content-input">
+          <input type="email" placeholder="Your email address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content-people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
     </div>
   )
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,4 +3,4 @@ export { default as Features } from './features/Features';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
 export { default as Possibility } from './possibility/Possibility';
-export { default as whatGPT3 } from './whatGPT3/whatGPT3';
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
